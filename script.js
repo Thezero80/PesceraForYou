dbdb0705d43b98eb00be77ac62abeb16655412f6
Thezero80 <150729225+Thezero80@users.noreply.github.com>
2024-07-30 22:29:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let intervalId = null;
 const fishbowl = document.getElementById('fishbowl');
 const fish = document.getElementById('fish');
 const tap = document.getElementById('tap');
+const mensaje = document.getElementById("mensaje")
 
 // Agrega elementos de audio
-const happyFishSound = new Audio('beso.mp3'); // Reemplaza 'beso.mp3' con la ruta a tu archivo de sonido
-const deadFishSound = new Audio('diadesuerte.mp3'); // Reemplaza 'deadfish.mp3' con la ruta a tu archivo de sonido
+const happyFishSound = new Audio('beso.mp3');
+const deadFishSound = new Audio('diadesuerte.mp3');
 
 const emptyingFn = () => setInterval(() => {
 	fill = fill - 1;
@@ -18,6 +19,7 @@ const emptyingFn = () => setInterval(() => {
 		fish.classList.add('fishbowl__fish--dead');
 		happyFishSound.pause();
 		deadFishSound.play(); // Reproduce el sonido del pez muriendo
+		mensaje.innerHTML = "<div style='color: whitesmoke;background-color: red;font-size: 40px;'>Necesito un poco de awita que me muero!</div>"
 	} else if (fill < 50) {
 		fish.classList.add('fishbowl__fish--dying');
 	} else {
@@ -25,6 +27,7 @@ const emptyingFn = () => setInterval(() => {
 		fish.classList.remove('fishbowl__fish--dead');
 		deadFishSound.pause();
 		happyFishSound.play(); // Reproduce el sonido del pez feliz
+		mensaje.innerHTML = "<div style='color: whitesmoke;background-color: green;font-size: 40px;'>No me dejes morir, Te quiero!</div>"
 	}
 }, 200);
 
@@ -32,7 +35,7 @@ intervalId = emptyingFn();
 
 tap.addEventListener('click', () => {
 	tap.classList.add('fishbowl__tap--active');
-	setTimeout(() => tap.classList.remove('fishbowl__tap--active'), 4000);
+	setTimeout(() => tap.classList.remove('fishbowl__tap--active'), 500);
 	if (fill <= 0) {
 		intervalId = emptyingFn();
 		fish.classList.add('fishbowl__fish--floating');
@@ -40,5 +43,21 @@ tap.addEventListener('click', () => {
 	fill = Math.min(fill + 20, 90);
 });
 
+function createBubble() {
+	const bubble = document.createElement('div');
+	bubble.classList.add('bubble');
+	fishbowl.appendChild(bubble);
+
+	// Animación de la burbuja subiendo
+	bubble.style.animation = 'bubbleAnimation 2s linear infinite';
+
+	// Eliminar la burbuja después de un tiempo
+	setTimeout(() => {
+		bubble.remove();
+	}, 2000);
+}
+
+// Crear burbujas aleatoriamente
+setInterval(createBubble, 500);
 
 
